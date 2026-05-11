8324b7f424535fed59dfdb5051dd4a34242429da
248uddin <qasimnaeemuddin8@gmail.com>
2026-05-11 17:54:37 -0400

    baseurl change

--- frontend/src/api/nbaApi.js
+++ frontend/src/api/nbaApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://127.0.0.1:8000" });
+const api = axios.create({ baseURL: "https://hooptracker-api.onrender.com" });
 
 export const getLiveScores = () => api.get("/games/live");
 export const getBoxscore = (gameId) => api.get(`/games/${gameId}/boxscore`);
